1434cb5a005fa864e2b8e98302407cc0696bbd1d
Adam Butt <adamb@gwu.edu>
2025-11-21 21:12:08 -0500

    Passed

--- ipc.c
+++ ipc.c
@@ -5,6 +5,17 @@
 
 pid_t fork_and_exec(char** exec_args, int pipe_read, int pipe_write) {                        
 
+    pid_t pid = fork();
 
+    if (pid == 0) {
+        close(pipe_read);
+        
+        //- redirect all stdout from child process to the write end of the pipe
+        dup2(pipe_write, STDOUT_FILENO);
+
+        close(pipe_write);
+
+        execvp(exec_args[0], exec_args);
+    }
     // don't forget to return the pid of the child 
 }
